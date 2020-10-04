7fb85e1ae2e3b616351194c902cab08c24d9e747
happysuperevil601 <happysuperevil601@gmail.com>
2020-10-04 22:00:08 +0200

    C_001_Created_Repository

---
+++ src/main/java/com/superdevs/HealthOMeter/HealthOMeterApplication.java
@@ -0,0 +1,13 @@
+package com.superdevs.HealthOMeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthOMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthOMeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superdevs/HealthOMeter/HealthOMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superdevs.HealthOMeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthOMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
